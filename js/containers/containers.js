56cf56cc8a808cd577d2a02f714b9222b664c1d7
DrRuski <ilia_e@hotmail.com>
2022-11-07 20:15:44 +0100

    commit

--- js/containers/containers.js
+++ js/containers/containers.js
@@ -1,4 +1,4 @@
 // Index - Home
 export const indexTopSection = document.querySelector(".topSection");
 export const indexMidSection = document.querySelector(".midSection");
-export const indexBottomSection = document.querySelector(".bottomSection")
+export const indexBottomSection = document.querySelector(".bottomSection");
--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
-import { indexBottomSection, indexMidSection, indexTopSection } from "./containers/containers";
+import {
+  indexBottomSection,
+  indexMidSection,
+  indexTopSection,
+} from "./containers/containers";
 
 const url = "";
 
 async function getBlogPosts() {
-    try {
-        const response = await fetch(url);
-        const blogPosts = await response.json();
-
-        
-    }
-    catch(error) {
-        console.log(error)
-    }
+  try {
+    const response = await fetch(url);
+    const blogPosts = await response.json();
+  } catch (error) {
+    console.log(error);
+  }
 }
