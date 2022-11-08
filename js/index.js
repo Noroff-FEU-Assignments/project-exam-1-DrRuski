a8a2b82df75713fb8d962fe34a8ed4a822aa64fa
DrRuski <ilia_e@hotmail.com>
2022-11-08 11:12:07 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -2,15 +2,26 @@ import {
   indexBottomSection,
   indexMidSection,
   indexTopSection,
-} from "./containers/containers";
+} from "./containers/containers.js";
 
-const url = "";
+const url = 'https://imdev.no/wp-json/wp/v2/posts?_embed';
 
 async function getBlogPosts() {
   try {
     const response = await fetch(url);
     const blogPosts = await response.json();
+
+    
+
+    for (let index = 0; index < blogPosts.length; index++) {
+      const element = blogPosts[index];
+      console.log(element)
+      
+      indexTopSection.innerHTML += `<div><h1>${element.title.rendered}</h1></div>`
+    }
   } catch (error) {
     console.log(error);
   }
 }
+
+getBlogPosts();
