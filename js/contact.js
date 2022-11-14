9e81e0a28b23c2a45e7f0dc91b52f29fe00c0d75
DrRuski <ilia_e@hotmail.com>
2022-11-14 09:52:38 +0100

    commit

--- js/contact.js
+++ js/contact.js
@@ -1,57 +1,16 @@
-import { checkLength, valEmail, formValid } from "./utility/formValidation.js";
-import {
-  form,
-  firstName,
-  lastName,
-  email,
-  firstNameError,
-  lastNameError,
-  emailError,
-  problemDescription,
-  problemDescriptionError,
-  formBtn,
-} from "./containers/containers.js";
-
-import {
-  buttonErrorStyle,
-  buttonAcceptedStyle,
-} from "./createHTML/formButtonDisplay.js";
+import { contactForm, formBtn } from "./containers/containers.js";
 
 function validForm(event) {
   event.preventDefault();
 
-  if (checkLength(firstName.value, 2) === true) {
-    firstNameError.style.display = "none";
-  } else {
-    firstNameError.style.display = "block";
-  }
-
-  if (checkLength(lastName.value, 2) === true) {
-    lastNameError.style.display = "none";
-  } else {
-    lastNameError.style.display = "block";
-  }
-
-  if (valEmail(email.value)) {
-    emailError.style.display = "none";
-  } else {
-    emailError.style.display = "block";
-  }
-
-  if (checkLength(problemDescription.value, 2) === true) {
-    problemDescriptionError.style.display = "none";
-  } else {
-    problemDescriptionError.style.display = "block";
-  }
-
-  btnStyle();
-}
-form.addEventListener("submit", validForm);
-
-function btnStyle() {
-  if (formValid) {
+  if (validForm) {
+    contactForm.reset();
     return buttonAcceptedStyle();
-  } else {
-    return buttonErrorStyle();
   }
+}
+contactForm.addEventListener("submit", validForm);
+
+function buttonAcceptedStyle() {
+  formBtn.style.backgroundColor = `rgb(40, 167, 69)`;
+  formBtn.innerText = `The form has been submitted! ✔`;
 }
--- js/containers/containers.js
+++ js/containers/containers.js
@@ -5,18 +5,5 @@ export const indexBottomSection = document.querySelector(".bottomSection");
 
 // Contact - Form
 
-export const firstName = document.querySelector("#firstName");
-export const lastName = document.querySelector("#lastName");
-export const email = document.querySelector("#email");
-export const problemDescription = document.querySelector("#problemDetails");
-
-export const form = document.querySelector(".contactMainContainer");
-
-export const firstNameError = document.querySelector("#firstNameError");
-export const lastNameError = document.querySelector("#lastNameError");
-export const emailError = document.querySelector("#emailError");
-export const problemDescriptionError = document.querySelector(
-  "#problemDetailsError"
-);
-
-export const formBtn = document.getElementById("formSubmitBtn");
+export const contactForm = document.querySelector(".contactMainContainer");
+export const formBtn = document.querySelector(".formSubmitBtn");
