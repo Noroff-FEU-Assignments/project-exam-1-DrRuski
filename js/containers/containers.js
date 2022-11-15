93fad2e0bccedada9abef3e3d5b2d477e37e9ea6
DrRuski <ilia_e@hotmail.com>
2022-11-15 12:47:47 +0100

    commit

--- js/containers/containers.js
+++ js/containers/containers.js
@@ -3,7 +3,9 @@ export const indexTopSection = document.querySelector(".topSection");
 export const indexMidSection = document.querySelector(".midSection");
 export const indexBottomSection = document.querySelector(".bottomSection");
 
-// Contact - Form
 
+// Contact - Form
 export const contactForm = document.querySelector(".contactMainContainer");
 export const formBtn = document.querySelector(".formSubmitBtn");
+
+
