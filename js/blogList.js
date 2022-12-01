3ed16d20cce51d4b54f154c6f78e256dbecf3f76
DrRuski <ilia_e@hotmail.com>
2022-12-01 17:05:45 +0100

    commit

--- js/blogList.js
+++ js/blogList.js
@@ -1,10 +1,4 @@
-import {
-  blogListContainer,
-  showMoreButton,
-  showLessButton,
-  showMoreBtnContainer,
-  additionalPosts,
-} from "./containers/containers.js";
+import { renderBlogPosts } from "./createHTML/renderBlogList.js";
 
 const url = "https://imdev.no/wp-json/wp/v2/posts?_embed&per_page=20";
 
@@ -12,74 +6,8 @@ async function getBlogPosts() {
   try {
     const response = await fetch(url);
     const blogPosts = await response.json();
-    
-    blogListContainer.innerHTML = "";
-    
-    blogPosts.map((post) => {
-      const postId = `${post.id}`;
-      const imgUrl = `${post._embedded["wp:featuredmedia"][0].source_url}`;
-      const postTitle = `${post.title.rendered}`;
-      const category = `${post._embedded["wp:term"][0]
-        .map((catg) => `${catg.name}`)
-        .join(" ")}`;
-      const postTags = `${post._embedded["wp:term"][1]
-        .map((tag) => `${tag.name}`)
-        .join(" ")}`;
-      const author = `${post._embedded.author.map(
-        (authorName) => authorName.name
-      )}`;
 
-      if (
-        !postTags.toLowerCase().startsWith("latest") &&
-        !postTags.toLowerCase().startsWith("popular")
-      ) {
-        blogListContainer.innerHTML += `
-                          <div class="blogPostWrapper flex-vert">
-                            <a href="individualBlog.html?id=${postId}">
-                              <img class="postImage" src="${imgUrl}">
-                              <p class="${postTags} blogPostTags">${postTags}</p>
-                              <h4>${postTitle}</h4>
-                            </a>
-                            <div class="flex-horiz">
-                              <p class="subtext">${author} | <a href="#" class="subtext" id="${category}">#${category}</a></p>
-                            </div>
-                          </div>`;
-      }
-
-      showLessButton.style.display = "none";
-
-      showMoreButton.addEventListener("click", () => {
-        showMoreButton.style.display = "none";
-        showLessButton.style.display = "block";
-        if (
-          postTags.toLowerCase().startsWith("latest") ||
-          postTags.toLowerCase().startsWith("popular")
-        ) {
-          additionalPosts.innerHTML += `
-                                    <div class="blogPostWrapper flex-vert">
-                                      <a href="individualBlog.html?id=${postId}">
-                                        <img class="postImage" src="${imgUrl}">
-                                        <p class="${postTags} blogPostTags">${postTags}</p>
-                                        <h4>${postTitle}</h4>
-                                      </a>
-                                      <div class="flex-horiz">
-                                        <p class="subtext">${author} | <a href="#" class="subtext" id="${category}">#${category}</a></p>
-                                      </div>
-                                    </div>`;
-        }
-      });
-
-      showLessButton.addEventListener("click", () => {
-        showMoreButton.style.display = "block";
-        showLessButton.style.display = "none";
-        if (
-          postTags.toLowerCase().startsWith("latest") ||
-          postTags.toLowerCase().startsWith("popular")
-        ) {
-          additionalPosts.innerHTML = "";
-        }
-      });
-    });
+    renderBlogPosts(blogPosts);
   } catch (error) {
     console.log(error);
   }
---
+++ js/createHTML/renderBlogList.js
@@ -0,0 +1,78 @@
+import {
+  blogListContainer,
+  additionalPosts,
+  showLessButton,
+  showMoreButton,
+} from "../containers/containers.js";
+
+export function renderBlogPosts(blogPosts) {
+  blogListContainer.innerHTML = "";
+
+  blogPosts.map((post) => {
+    const postId = `${post.id}`;
+    const imgUrl = `${post._embedded["wp:featuredmedia"][0].source_url}`;
+    const postTitle = `${post.title.rendered}`;
+    const category = `${post._embedded["wp:term"][0]
+      .map((catg) => `${catg.name}`)
+      .join(" ")}`;
+    const postTags = `${post._embedded["wp:term"][1]
+      .map((tag) => `${tag.name}`)
+      .join(" ")}`;
+    const author = `${post._embedded.author.map(
+      (authorName) => authorName.name
+    )}`;
+
+    if (
+      !postTags.toLowerCase().startsWith("latest") &&
+      !postTags.toLowerCase().startsWith("popular")
+    ) {
+      blogListContainer.innerHTML += `
+                        <div class="blogPostWrapper flex-vert">
+                          <a href="individualBlog.html?id=${postId}">
+                            <img class="postImage" src="${imgUrl}">
+                            <p class="${postTags} blogPostTags">${postTags}</p>
+                            <h4>${postTitle}</h4>
+                          </a>
+                          <div class="flex-horiz">
+                            <p class="subtext">${author} | <a href="#" class="subtext" id="${category}">#${category}</a></p>
+                          </div>
+                        </div>`;
+    }
+
+    showLessButton.style.display = "none";
+
+    showMoreButton.addEventListener("click", () => {
+      showMoreButton.style.display = "none";
+      showLessButton.style.display = "block";
+
+      if (
+        postTags.toLowerCase().startsWith("latest") ||
+        postTags.toLowerCase().startsWith("popular")
+      ) {
+        additionalPosts.innerHTML += `
+                                  <div class="blogPostWrapper flex-vert">
+                                    <a href="individualBlog.html?id=${postId}">
+                                      <img class="postImage" src="${imgUrl}">
+                                      <p class="${postTags} blogPostTags">${postTags}</p>
+                                      <h4>${postTitle}</h4>
+                                    </a>
+                                    <div class="flex-horiz">
+                                      <p class="subtext">${author} | <a href="#" class="subtext" id="${category}">#${category}</a></p>
+                                    </div>
+                                  </div>`;
+      }
+    });
+
+    showLessButton.addEventListener("click", () => {
+      showMoreButton.style.display = "block";
+      showLessButton.style.display = "none";
+
+      if (
+        postTags.toLowerCase().startsWith("latest") ||
+        postTags.toLowerCase().startsWith("popular")
+      ) {
+        additionalPosts.innerHTML = "";
+      }
+    });
+  });
+}
